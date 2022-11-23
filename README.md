8cc2f6c4773f89be598038efdf18d416062cdfd0
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-11-22 20:58:07 -0500

    finished function edits in javascript and wrote readme file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Server-Side-API-Weather-App
+  [![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+  ## Table of Contents
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Licenses](#License)
+ 
+  ## Description
+  The weather dashboard is a browser-based application with dynamically updated HTML and CSS. To access both current and anticipated weather data, it makes use of Third-Party APIs provided by OpenWeather.com.The app makes requests to a URL with particular parameters through APIs.
+  ## Installation
+  Clone the repository locally and double click index.html or click on the link below in the usage section
+
+## Screenshot
+ ![Website Screenshot](.//assets/images/websitescreenshot.jpeg)
+
+
+  ## Usage
+  The current and predicted weather conditions are shown to the user's current location when they open the app. If the user so chooses, they can search for the conditions in other locations. The other locations are returned to the user for future use and stored in local storage. The current weather and five-day forecast will be displayed by the application.
+  To visit please click on link [link](https://vsoto7697.github.io/Server-Side-API-Weather-App/)
+  ## License  
+  This project is covered under the No License license.
+ 
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/Vsoto7697  
+  Email: Vsoto7697@gmail.com
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var forecastDiv = $("#Forecastweather");
 var citiesArray;
 
 // City search when the user clicks the search icon
-$("#Citysubmit").click(function() {
+$("#Citysubmit").click(function () {
     event.preventDefault();
     let cityName = $("#Inputcity").val();
     returnCurrentWeather(cityName);
@@ -16,7 +16,7 @@ $("#Citysubmit").click(function() {
 });
 
 // Previous Cities will show under search 
-$("#Searchprevious").click(function() {
+$("#Searchprevious").click(function () {
     let cityName = event.target.value;
     returnCurrentWeather(cityName);
     returnWeatherForecast(cityName);
@@ -31,7 +31,7 @@ if (localStorage.getItem("localWeatherSearches")) {
 };
 
 // added a function to Clear Local Storage whenever user clicks clear
-$("#clear").click(function() {
+$("#clear").click(function () {
     localStorage.clear('localWeatherSearches');
 });
 
@@ -39,14 +39,14 @@ $("#clear").click(function() {
 function returnCurrentWeather(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
 
-    $.get(queryURL).then(function(response){
+    $.get(queryURL).then(function (response) {
         // Current Date 
-        let currTime = new Date(response.dt*1000);
+        let currTime = new Date(response.dt * 1000);
         // Displays Weather Icon 
         let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
         currWeatherDiv.html(`
-        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth() + 1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
         <p>Temperature: ${response.main.temp}&#176;F</p>
         <p>Humidity: ${response.main.humidity}%</p>
         <p>Wind Speed: ${response.wind.speed} mph</p>
@@ -59,15 +59,15 @@ function returnCurrentWeather(cityName) {
 function returnWeatherForecast(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&APPID=ef97f3de9f468cfe0aff41d0a911b1a4`;
 
-    $.get(queryURL).then(function(response){
+    $.get(queryURL).then(function (response) {
         let forecastInfo = response.list;
         forecastDiv.empty();
-        $.each(forecastInfo, function(i) {
+        $.each(forecastInfo, function (i) {
             if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
                 return;
             }
             // dates for forecast
-            let forecastDate = new Date(forecastInfo[i].dt*1000);
+            let forecastDate = new Date(forecastInfo[i].dt * 1000);
             // display weather Icon
             let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
             // When searching, append data to a five-day forecast.
@@ -75,7 +75,7 @@ function returnWeatherForecast(cityName) {
             <div class="col-md">
                 <div class="card text-white bg-primary">
                     <div class="card-body">
-                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <h4>${forecastDate.getMonth() + 1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
                         <img src=${weatherIcon} alt="Icon">
                         <p>Temp: ${forecastInfo[i].main.temp}&#176;F</p>
                         <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
@@ -86,6 +86,56 @@ function returnWeatherForecast(cityName) {
         })
     })
 };
+
+// The current weather and the UV index are collected simultaneously.
+function returnUVIndex(coordinates) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function (response) {
+        let currUVIndex = response.value;
+        let uvSeverity = "green";
+        let textColour = "white"
+
+        //The intensity of the UV will change the background color based on below criteria.
+        // This is for calculating the UV Index.
+        if (currUVIndex >= 11) {
+            uvSeverity = "purple";
+        } else if (currUVIndex >= 8) {
+            uvSeverity = "red";
+        } else if (currUVIndex >= 6) {
+            uvSeverity = "orange";
+            textColour = "black"
+        } else if (currUVIndex >= 3) {
+            uvSeverity = "yellow";
+            textColour = "black"
+        }
+        currWeatherDiv.append(`<p>UV Index: <span class="text-${textColour} uvPadding" style="background-color: ${uvSeverity};">${currUVIndex}</span></p>`);
+    })
+}
+
+// Creates a history of recent searches based on user's local storage
+function createHistoryButton(cityName) {
+    var citySearch = cityName.trim();
+    var buttonCheck = $(`#Searchprevious > BUTTON[value='${citySearch}']`);
+    if (buttonCheck.length == 1) {
+        return;
+    }
+
+    if (!citiesArray.includes(cityName)) {
+        citiesArray.push(cityName);
+        localStorage.setItem("localWeatherSearches", JSON.stringify(citiesArray));
+    }
+
+    $("#Searchprevious").prepend(`
+    <button class="btn btn-light cityHistoryBtn" value='${cityName}'>${cityName}</button>
+    `);
+}
+
+function writeSearchHistory(array) {
+    $.each(array, function (i) {
+        createHistoryButton(array[i]);
+    })
+}
 
 
 
