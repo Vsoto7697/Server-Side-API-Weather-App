aa37a294fd0d919d3743f382f0d131480001e6b6
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-11-22 20:30:41 -0500

    called api to retrieve current weather infomation

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,4 +35,57 @@ $("#clear").click(function() {
     localStorage.clear('localWeatherSearches');
 });
 
+// call API to retrieve current weather information by city name //
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        // Current Date 
+        let currTime = new Date(response.dt*1000);
+        // Displays Weather Icon 
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currWeatherDiv.html(`
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp}&#176;F</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} mph</p>
+        `, returnUVIndex(response.coord))
+        createHistoryButton(response.name);
+    })
+};
+
+// Call API for weather forecast 
+function returnWeatherForecast(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&APPID=ef97f3de9f468cfe0aff41d0a911b1a4`;
+
+    $.get(queryURL).then(function(response){
+        let forecastInfo = response.list;
+        forecastDiv.empty();
+        $.each(forecastInfo, function(i) {
+            if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+                return;
+            }
+            // dates for forecast
+            let forecastDate = new Date(forecastInfo[i].dt*1000);
+            // display weather Icon
+            let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
+            // When searching, append data to a five-day forecast.
+            forecastDiv.append(`
+            <div class="col-md">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon} alt="Icon">
+                        <p>Temp: ${forecastInfo[i].main.temp}&#176;F</p>
+                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        })
+    })
+};
+
+
 
